470f0b648400c01ba18ae91b5093ee92f5c59252
Evgeniy Pomnikov <eugeun@mail.ru>
2017-02-27 13:36:25 +0300

    Add test global library

---
+++ vars/myTest.groovy
@@ -0,0 +1,15 @@
+#!/bin/groovy
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    if (config.hello == null) {
+      config.hello = 'Greetings'
+    }
+
+    echo "${config.hello}, world!"
+
+}
